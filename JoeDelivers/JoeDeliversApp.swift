1b9b2985763b60aa6d82492edd216691ae0507e6
kai tran <ultra1524@gmail.com>
2024-03-11 18:10:49 -0700

    Initial Commit

---
+++ JoeDelivers/JoeDeliversApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoeDeliversApp.swift
+//  JoeDelivers
+//
+//  Created by Triet Tran on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct JoeDeliversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
